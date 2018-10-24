f83be2b753ef1a9ca97d6598ddef024c39224bab
corey-code <cmmcorey@gmail.com>
2018-10-23 21:04:13 -0600

    bitcore-lib-vidulum from git

--- explorer.sh
+++ explorer.sh
@@ -24,7 +24,7 @@ sudo n 4
 sudo apt-get -y install libzmq3-dev
 
 # install vidulum version of bitcore
-npm install vidulum/bitcore-node-vidulum
+npm install git+https://git@github.com/vidulum/bitcore-lib-vidulum.git
 
 # create bitcore node
 ./node_modules/bitcore-node-vidulum/bin/bitcore-node create vidulum-explorer
